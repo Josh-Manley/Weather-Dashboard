3fc0424b62d56795f2430b40e7a9ff564eb33470
Josh-Manley <joshmanley45@gmail.com>
2024-02-02 16:09:15 -0700

    added 5 day forecast

--- script/script.js
+++ script/script.js
@@ -1,9 +1,10 @@
 let fetchButton = document.getElementById('fetch-button');
 let cityName = document.getElementById('search-city-input');
 let mainCityCard = document.getElementById('main-city-info')
+let forecastContainer = document.getElementById('forecast-container')
 
 function getApi() {
-  let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName.value}&cnt=3&appid=202ed1305c35eb4c1d3fc781601f9daf&units=imperial`;
+  let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName.value}&cnt=5&appid=202ed1305c35eb4c1d3fc781601f9daf&units=imperial`;
 
   fetch(requestUrl)
   .then(function (response) {
@@ -17,7 +18,21 @@ function getApi() {
     <p>Tempature: ${data.list[0].main.temp}&#176;F</p>
     <p>Wind: ${data.list[0].wind.speed} MPH</p>
     <p>Humidity: ${data.list[0].main.humidity} %</p>
-    `
+    `;
+
+    // Display 5-day forecast information
+    forecastContainer.innerHTML = '';
+    for (let i = 1; i <= 5; i++) {
+      forecastContainer.innerHTML += `
+        <div class="forecast-day">
+          <p>Date: ${data.list[i].dt_txt}</p>
+          <p>Temperature: ${data.list[i].main.temp}&deg;F</p>
+          <p>Humidity: ${data.list[i].main.humidity}%</p>
+          <p>Wind Speed: ${data.list[i].wind.speed} mph</p>
+          <!-- Add icon representation of weather conditions -->
+        </div>
+      `;
+    }    
   })
 }
 
